ddd57ad7b510bc4f591818ed42dba5efcab6846a
Monica Liz Rodriguez <monica.liz.rodriguez@gmail.com>
2021-05-18 01:12:47 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -5,7 +5,7 @@ def valid_move?(board, index)
   if index.between?(1, 9)
     if !position_taken?(board, index)
     return true
-  else position_taken?(board, index)
+  else 
     return false
   end
 end
