43cb373c99d340302d52404435c8d797e3d9fa7f
Monica Liz Rodriguez <monica.liz.rodriguez@gmail.com>
2021-05-18 01:25:24 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,13 +1,14 @@
 # code your #valid_move? method here
+require 'pry'
 def valid_move?(board, index)
-  if index.between?(1, 9)
-    if !position_taken?(board, index)
+  # binding.pry
+  if index.between?(0, 8) && !position_taken?(board, index)
     return true
-  else position_taken?(board, index)
+  else
     return false
   end
 end
-end
+
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 
